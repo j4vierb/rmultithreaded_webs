72ce2b29523b137ee27a15f13c52d329de606636
j4vierb <js.barrerat1@uniandes.edu.co>
2025-08-02 21:20:44 -0500

    Upload progress with the ThreadPool in 'Turning Our Single-Threaded Server into a Multithreaded Server' section

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     fs,
+    thread,
+    time::Duration, 
 };
+
+use rmultithreaded_webs::ThreadPool;
 
 fn main() {
     println!("Hello, world!");
@@ -10,24 +14,40 @@ fn main() {
         Err(e) => panic!("ERROR: the port is already being use!"),
         Ok(tcp_listener) => tcp_listener,
     };
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         // when stream goes out of scope it's dropped by the Drop trait impl
         let stream = stream.unwrap();
 
         println!("Connection established!");
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream)
+        });
     }
 }
+
+/*
+* [*>] Thread pool 
+*
+* This technique is just one of many ways to improve the throughput of a web server.
+* Other options you might explore are the fork/join model, the single-threaded async 
+* I/O model, and the multithreaded async I/O model. If you’re interested in this topic,
+* you can read more about other solutions and try to implement them; with a low-level
+* language like Rust, all of these options are possible.
+* */
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
